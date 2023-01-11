8728683fe8d215cefcc2f0ce8c835af0c5523b23
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-01-10 20:31:49 -0600

    fixed borkedness

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
 import List from "./List";
+import Description from "./Description";
 
 export default function App() {
   return (
