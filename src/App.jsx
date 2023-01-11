88ccaabe29aa54a20f35bb00f591063ec8f42357
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-01-10 19:49:53 -0600

    remove copyright

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import Copyright from './Copyright';
 
 export default function App() {
   return (
@@ -16,8 +15,6 @@ export default function App() {
           is an American writer and web developer. He is the creator of <Link href="https://www.thegoodliferevival.com/"><em>The Good Life Revival Podcast</em></Link> (2016-2019) and the author of two books: <Link href="https://shop.sycamore.garden/l/introduction-to-foraging"><em>Introduction to Foraging</em></Link> (2018) and <Link href="https://www.thegoodliferevival.com/shop/fna"><em>Foraging North America</em></Link> (2019).
           He currently works as a technical writer and developer relations engineer. View <Link href="https://work.sycamore.garden/">Sam's professional portfolio</Link>.
         </Typography>
-        
-        <Copyright />
       </Box>
     </Container>
   );
